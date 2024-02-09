b05a7814fa1d93c14a8cc21461399d90e9ea0cf1
Kirill Shapovalov <r0dney@outlook.com>
2024-02-10 01:54:42 +0700

    terraform init

---
+++ diplom.tf/vars.tf
@@ -0,0 +1,18 @@
+####################################
+### -------- Cloud Vars -------- ###
+####################################
+
+variable "token" {
+  type        = string
+  description = "OAuth-token"
+}
+
+variable "cloud_id" {
+  type        = string
+  description = "Yandex.Cloud ID"
+}
+
+variable "folder_id" {
+  type        = string
+  description = "Yandex.Cloud MyDefault folder ID"
+}
